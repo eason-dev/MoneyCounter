5d2aef87e6e0b06e8be38a6c09463f2079f75ba1
Eason Chang <eason@easonchang.com>
2024-12-29 19:50:19 -0700

    Initial Commit

---
+++ MoneyCounterTests/MoneyCounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyCounterTests.swift
+//  MoneyCounterTests
+//
+//  Created by Eason on 12/29/24.
+//
+
+import Testing
+@testable import MoneyCounter
+
+struct MoneyCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ MoneyCounterUITests/MoneyCounterUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MoneyCounterUITestsLaunchTests.swift
+//  MoneyCounterUITests
+//
+//  Created by Eason on 12/29/24.
+//
+
+import XCTest
+
+final class MoneyCounterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
